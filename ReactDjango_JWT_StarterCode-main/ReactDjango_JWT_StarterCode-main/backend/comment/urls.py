501c867c7f21dac9fd810dfb2ee18e6c19177f48
Niguel Price <niguelprice@yahoo.com>
2022-12-19 04:12:06 -0500

    changed python interpreter, added functionality for get and post request

---
+++ ReactDjango_JWT_StarterCode-main/ReactDjango_JWT_StarterCode-main/backend/comment/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from comment import views
+
+# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
+urlpatterns = [
+    path('', views.user_comments),
+    path('all/', views.get_all_comments),
+]
